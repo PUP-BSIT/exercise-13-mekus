5c50ab0fb05e72fd365c149bfdb50df8b06fed55
agulto-jermainerazehl <agultorazehl@gmail.com>
2025-06-04 23:18:18 +0800

    update agulto module exit message

--- modules/agulto.py
+++ modules/agulto.py
@@ -72,7 +72,7 @@ def agulto_main():
         choice = get_user_choice()
 
         if choice == 0:
-            print("Exiting the program. Goodbye!\n")
+            print("Exiting the module. Goodbye!\n")
             break
 
         handle_user_choice(choice)
